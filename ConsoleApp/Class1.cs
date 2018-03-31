14df219525a7218dcad69e119edce8ec06fec597
alexander.shabatura <alexander.shabatura@gmail.com>
2018-03-31 12:04:00 +0300

    клиентские изменения 1

---
+++ ConsoleApp/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+	class Class1
+	{
+		/* клиентские изменения 1*/
+	}
+}
